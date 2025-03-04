0beb932c4f31e0767b966244e45153955db0cad4
Hamza BEJAOUI <hamzabejaoui2000@outlook.com>
2025-03-04 09:40:27 +0100

    feat(auth): add middleware for password hashing & comparison function
     Added middleware to hash user passwords before storing them
     Implemented a function to compare hashed passwords with user input

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -20,8 +21,31 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  password: {
+    type: String,
+    required: true,
+  },
 });
+
+// Pre-save middleware to hash the password
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+      next();
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next();
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = User; // Assure-toi que cette ligne est bien présente
+module.exports = User;
